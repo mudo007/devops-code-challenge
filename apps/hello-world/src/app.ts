3ce53b671896877ea41cb21d72ec49ed68f317c3
Diogo Andrade <diogo007@gmail.com>
2024-07-08 20:26:16 -0300

    Fix de linter para a aplicação

--- apps/hello-world/src/app.ts
+++ apps/hello-world/src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express, { Request, Response } from 'express';
 
 const app = express();
@@ -8,7 +9,9 @@ app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 app.get('/health/check', (req: Request, res: Response) => res.send('OK'));
 
 // Endpoint bobo para dar trigger no pipeline de CI/CD
-app.get('/chuck-norris', (req: Request, res: Response) => res.send('Round House Kicks you on the face'));
+app.get('/chuck-norris', (req: Request, res: Response) =>
+  res.send('Round House Kicks you on the face')
+);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
